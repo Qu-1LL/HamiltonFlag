3aed9df85d04229a86c089019fb257103f64abca
xXSpaghett69Xx <qjm412@gmail.com>
2025-09-05 18:13:31 -0400

    updated API code

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const port = 3000
 app.use(express.json())
 
 app.use(cors({
-    origin: 'http://localhost:5000'
+    origin: `https://HamiltonFlagFootball:${port}`
 }))
 
 const upload = multer({ storage: multer.memoryStorage() })
