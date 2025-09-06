40d125c8109361868f3521c6823da5beda42009e
xXSpaghett69Xx <qjm412@gmail.com>
2025-09-06 00:50:14 -0400

    updated starts cript

--- app/react-server.js
+++ app/react-server.js
@@ -4,15 +4,12 @@ const path = require('path');
 const app = express();
 const PORT = 5000;
 
-// Serve static React files
 app.use(express.static(path.join(__dirname, 'build')));
 
-// Redirect all routes to index.html (for React Router)
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-// Listen on all interfaces
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`React app running on http://0.0.0.0:${PORT}`);
 });
